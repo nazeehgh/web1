5fbae8cb9d129c5b02c71b2cdd974aab9dbd3005
nazeehgh <nazeehgh@gmail.com>
2025-04-29 11:15:31 +0300

    id page

--- res/javaScript.js
+++ res/javaScript.js
@@ -1,12 +1,15 @@
-function showHideByClassName() {
-    var x = document.getElementsByClassName("web1");
-    for (var i = 0; i < x.length; i++) {
-      if(x[i].style.display == "none")
-      {
-              x[i].style.display = "block";
-      }
-      else{
+function showHideweb1Class() {
+  var x = document.getElementsByClassName("web1");
+  for (var i = 0; i < x.length; i++) {
+    if (x[i].style.display == "none") {
+      x[i].style.display = "block";
+    }
+    else {
       x[i].style.display = "none";
-      }
     }
   }
+}
+
+function displayResultbyElementId() {
+  document.getElementById("div2").innerHTML = "This is content is changed using id=div2";
+}
